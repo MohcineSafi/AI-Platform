d5c86c2665e79285f4698ea7ba69b3a83e7c316a
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-31 18:05:56 +0000

    Feat:Add isMounted state, effect in mobile sidebar component and pathname in sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -66,6 +66,8 @@ const routes = [
 const Sidebar = () => {
   // const location = useLocation();
   // const pathname = location.pathname;
+  const pathname = usePathname();
+
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -83,7 +85,10 @@ const Sidebar = () => {
               key={route.href}
               href={route.href}
               className={cn(
-                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
               )}
             >
               <div className="flex items-center flex-1">
